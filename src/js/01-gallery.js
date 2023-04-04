218be3ff0ca003c9be550087f9d624f0330c4b00
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-04-04 17:14:44 +0200

    add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,29 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+  })
+  .join('');
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
