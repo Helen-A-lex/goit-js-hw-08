d4fd60422b139d77f2563b715841fc197ac78d0f
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-04-06 23:31:11 +0200

    add task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,27 +1,45 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-// const player = new Player('handstick', {
+
+// const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+// const iframe = document.querySelector('iframe');
+// console.log(iframe);
+// const player = new Player(iframe, {
 //   id: 19231868,
 //   width: 640,
 // });
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+// const getCurrentTime = function (currentTime) {
+//   const seconds = currentTime.seconds;
+//   console.log(currentTime);
+
+//   localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+// };
+
+// player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+// player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
 
 const iframe = document.querySelector('iframe');
-iframe.addEventListener('click');
-const player = new Vimeo.Player(iframe);
-const onTimeUpdate = function (data) {
-  {
-    duration: 61.857;
-    percent: 0.049;
-    seconds: 3.034;
-  }
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-player.on('timeupdate', onTimeUpdate);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime().then(function (title) {
-  console.log('title:', title);
-});
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
+const feedbackFormEl = document.querySelector('.feedback-form');
 
+feedbackFormEl.addEventListener('submit', onFeedbackForm);
+function onFeedbackForm(evt) {
+  evt.preventDefault();
+  const formData = new FormData(evt.currentTarget);
+  const data = {};
+  formData.forEach((value, name) => {
+    data[name] = value;
+  });
+  console.log(data);
+  //   localStorage.clear();
+  evt.currentTarget.reset();
+}
+
+feedbackFormEl.addEventListener('input', throttle(onFeedbackFormInput, 500));
+function onFeedbackFormInput(evt) {
+  const FormElements = evt.currentTarget.elements;
+
+  let email = FormElements.email.value.trim();
+
+  let textarea = FormElements.message.value.trim();
+  const formData = {
+    email: email,
+    message: textarea,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+
+  if (localStorage.getItem('email') && localStorage.getItem('message')) {
+    email = localStorage.getItem('email');
+    textarea = localStorage.getItem('message');
+  } else {
+    email === '' && textarea === '';
+  }
+}
+//  const storedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+//  if (storedData && storedData.email && storedData.message) {
+//    email = storedData.email;
+//    textarea = storedData.message;
+//  }
+
+//  FormElements.email.value = email;
+//  FormElements.message.value = textarea;
